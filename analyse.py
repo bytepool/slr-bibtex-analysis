5880028b08e676bb498261fa7e34849f59fc4e87
Aljoscha Lautenbach <aljoscha.lautenbach@gmail.com>
2018-07-06 18:16:08 +0200

    analyse.py: Elaborate on plans in comments.

--- analyse.py
+++ analyse.py
@@ -1,6 +1,35 @@
 #!/usr/bin/env python3
 
+# Process a single bibtex file (can contain the results of a single query, or all results combined)
+# We extract:
+#  - number of entries
+#  - word cloud for all titles
+#  - word cloud for all abstracts
+#  - number of papers per author
+#  - number of papers per publication venue
+
+#  - number of papers per organisation
+#  - number of people per organisation
+#  - number of papers per country
+#  - number of people per country
+#  
+# This implies a second DB with author affiliations
+# Also consider to feed author names back into query search. 
+#
+# Write the output to a simple csv file. 
+
+# csv header:
+# Query; DOI: Author; PublicationType; PublicationVenue; PublicationYear; Title; Abstract; WordCount;
+
+# Pub. Type can be Journal or Conference. 
+# Query is the query string which lead to this result. 
+# DOI is document object identifier:
+#   should be unique for every scientific publication (see crossref.com). 
+
 import bibtexparser
+
+JOURNAL = "Journal"
+CONFERENCE = "Conference"
 
 
 def preprocess(entry):
@@ -49,4 +78,21 @@ def __main__():
 
 if __name__ == "__main__":
     __main__()
+
+
+
+
+class Article():
+    """Datastructure for an article. """
+    def __init__(self, query_string="", authors=None, pub_type="", year=0, title="", abstract="", word_count=0, doi=""):
+        self.query_string = query_string
+        self.doi = doi
+        self.authors = authors
+        self.type = pub_type
+        self.year = year
+        self.title = title
+        self.abstract = abstract
+        self.word_count = word_count
+
+    # Query; DOI: Authors; PublicationType; PublicationVenue; PublicationYear; Title; Abstract; WordCount;
 
